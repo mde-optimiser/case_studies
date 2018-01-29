575879f8c919a24561c68173b27c10103f952e4e
alxbrd <alex+git@onboot.org>
2018-01-29 15:51:54 +0000

    Removed other case studies

--- service-composition/mdeo/uk.ac.kcl.mdeoptimise.service-composition.surrogates/src/main/java/models/service/composition/surrogates/ISurrogateModelsWrapper.java
+++
@@ -1,8 +0,0 @@
-package models.service.composition.surrogates;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public interface ISurrogateModelsWrapper {
-	List<Double> evaluate(ArrayList<Integer> values);
-}
